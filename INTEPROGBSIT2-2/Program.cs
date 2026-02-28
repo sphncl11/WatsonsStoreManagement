f1f6c0d19faeb73cf401593cc9b05867751da3cd
sphncl11 <sophianicolesalmendral11@gmail.com>
2026-02-28 11:01:13 +0800

    Add files via upload

---
+++ INTEPROGBSIT2-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace INTEPROGBSIT2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
